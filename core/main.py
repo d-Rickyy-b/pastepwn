44c3f76c8c7fcfee2e7c4999e3a05f1a6f0bcf4b
Rico <d-Rickyy-b@users.noreply.github.com>
2018-09-06 01:46:04 +0200

    FEATURE: Core functionality

---
+++ core/main.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+import os
+import logging
+import logging.handlers
+
+logdir_path = os.path.dirname(os.path.abspath(__file__))
+logfile_path = os.path.join(logdir_path, "logs", "bot.log")
+
+if not os.path.exists(os.path.join(logdir_path, "logs")):
+    os.makedirs(os.path.join(logdir_path, "logs"))
+
+logfile_handler = logging.handlers.WatchedFileHandler(logfile_path, 'a', 'utf-8')
+
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO, handlers=[logfile_handler])
+
+# Use long polling for fetching the pastes
+
+#
+
