e75ed80187ba8c3fd25a0c5f433c5f6c0a8ef941
Rico <d-Rickyy-b@users.noreply.github.com>
2018-09-06 23:31:55 +0200

    FEATURE: Add imports to init files in packages
    Also rename the exception file

---
+++ scraping/pastebin/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+
+from .pastebinscraper import PastebinScraper
+
+__all__ = ('PastebinScraper')
---
+++ scraping/pastebin/exceptions/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+
+from .ipnotregisterederror import IPNotRegisteredError
+
+__all__ = ('IPNotRegisteredError',)
