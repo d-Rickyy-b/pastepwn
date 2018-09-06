ac23a04974792d5a5d1ef675711e50432b934b46
Rico <d-Rickyy-b@users.noreply.github.com>
2018-09-07 00:11:59 +0200

    TASK: Make class out of pastebinScraper

--- scraping/pastebin/pastebinscraper.py
+++ scraping/pastebin/pastebinscraper.py
@@ -1,50 +1,81 @@
 # -*- coding: utf-8 -*-
 import json
+import logging
+
+import urllib3
+
+from paste import Paste
+from scraping import BasicScraper
+from scraping.pastebin.exceptions import IPNotRegisteredError, EmptyBodyException
+
+
 # https://pastebin.com/doc_scraping_api#2
 # Your whitelisted IP should not run into any issues as long as you don't abuse our service.
 # We recommend not making more than 1 request per second, as there really is no need to do so.
 # Going over 1 request per second won't get you blocked, but if we see excessive unnecessary scraping, we might take action.
 
-api_base_url = "https://scrape.pastebin.com/"
 
+class PastebinScraper(BasicScraper):
+    api_base_url = "https://scrape.pastebin.com"
 
-def check_error(body):
-    """Checks and returns True if an error occurred"""
-    if "DOES NOT HAVE ACCESS" in body:
-        return True
+    def __init__(self):
+        super().__init__()
+        self.logger = logging.getLogger(__name__)
 
+    @staticmethod
+    def check_error(body):
+        """Checks if an error occurred and raises an exception if it did"""
+        if body is None:
+            raise EmptyBodyException()
 
-def get_recent(limit=10):
-    endpoint = "api_scraping.php"
-    api_url = api_base_url + endpoint
+        if "DOES NOT HAVE ACCESS" in body:
+            raise IPNotRegisteredError()
 
-    try:
+    def get_recent(self, limit=10):
+        endpoint = "api_scraping.php"
+        api_url = "{0}/{1}?limit={2}".format(self.api_base_url, endpoint, limit)
+
+        try:
+            http = urllib3.PoolManager()
+            response = http.request('GET', api_url)
+            response_data = response.data.decode("utf-8")
+
+            self.check_error(response_data)
+
+            pastes_dict = json.loads(response_data)
+            pastes = []
+
+            for paste in pastes_dict:
+                paste_obj = Paste(key=paste.get("key"),
+                                  title=paste.get("title"),
+                                  user=paste.get("user"),
+                                  size=paste.get("size"),
+                                  date=paste.get("date"),
+                                  expire=paste.get("expire"),
+                                  syntax=paste.get("syntax"),
+                                  scrape_url=paste.get("scrape_url"),
+                                  full_url=paste.get("full_url"))
+                pastes.append(paste_obj)
+
+            return pastes
+        except Exception as e:
+            self.logger.error(e)
+
+    def get_paste(self, key):
+        endpoint = "api_scrape_item.php"
+        api_url = "{0}/{1}?i={2}".format(self.api_base_url, endpoint, key)
+        paste = None
+
+        try:
+            http = urllib3.PoolManager()
+            response = http.request('GET', api_url)
+            response_data = response.data.decode("utf-8")
+
+            self.check_error(response_data)
+        except Exception as e:
+            self.logger.error(e)
+
+        return paste
+
+    def scrape(self):
         pass
-        # Send request to pastebin
-        # Check for errors
-        # Load json to dict | json.loads()
-    except:
-        pass
-
-    # return dict
-
-
-def get_paste(key):
-    endpoint = "api_scrape_item.php"
-    api_url = api_base_url + endpoint
-    paste = None
-
-    try:
-        pass
-        # fetch content
-    except:
-        pass
-
-    return paste
-
-
-
-# Error message:
-# YOUR IP: xxx.xxx.xxx.xxx DOES NOT HAVE ACCESS.
-#
-# VISIT: https://pastebin.com/doc_scraping_api TO GET ACCESS!
