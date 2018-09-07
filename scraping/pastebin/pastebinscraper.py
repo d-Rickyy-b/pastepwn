30f595aeef60246897ea409dbb6287e48dd4bc68
Rico <d-Rickyy-b@users.noreply.github.com>
2018-09-07 23:29:49 +0200

    TASK: Implement scraping thread for pastebin

--- scraping/pastebin/pastebinscraper.py
+++ scraping/pastebin/pastebinscraper.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import json
 import logging
+import time
 
 import urllib3
 
@@ -16,11 +17,15 @@ from scraping.pastebin.exceptions import IPNotRegisteredError, EmptyBodyExceptio
 
 
 class PastebinScraper(BasicScraper):
+    name = "PastebinScraper"
     api_base_url = "https://scrape.pastebin.com"
 
     def __init__(self):
         super().__init__()
         self.logger = logging.getLogger(__name__)
+        self._last_scrape_time = 0
+        self._known_pastes = []
+        self._known_pastes_limit = 1000
 
     @staticmethod
     def _check_error(body):
@@ -45,6 +50,7 @@ class PastebinScraper(BasicScraper):
             pastes_dict = json.loads(response_data)
             pastes = []
 
+            # Loop through the response and create objects by the data
             for paste in pastes_dict:
                 paste_obj = Paste(key=paste.get("key"),
                                   title=paste.get("title"),
@@ -66,6 +72,8 @@ class PastebinScraper(BasicScraper):
         api_url = "{0}/{1}?i={2}".format(self.api_base_url, endpoint, key)
         paste = None
 
+        self.logger.debug("Downloading paste")
+
         try:
             http = urllib3.PoolManager()
             response = http.request('GET', api_url)
@@ -77,5 +85,34 @@ class PastebinScraper(BasicScraper):
 
         return paste
 
-    def scrape(self):
-        pass
+    def start(self):
+        while True:
+            self._last_scrape_time = int(time.time())
+            pastes = self._get_recent(limit=100)
+            for paste in pastes:
+                # check if paste is in list of known pastes
+                if paste.key in self._known_pastes:
+                    # Do nothing, if it's already known
+                    continue
+
+                # if paste is not known, download the body and put it on the queue and into the list
+                # TODO this request must be limited! (1 per second)
+                # Maybe move into own thread
+                paste.set_body(self._get_paste(paste.key))
+
+                self._known_pastes.append(paste.key)
+
+            # Delete some of the last pastes to not run into memory/performance issues
+            if len(self._known_pastes) > 1000:
+                start_index = len(self._known_pastes)-self._known_pastes_limit
+                self._known_pastes = self._known_pastes[start_index:]
+
+            # check if time since last
+            current_time = int(time.time())
+            diff = current_time - self._last_scrape_time
+
+            # if the last scraping happened less than 60 seconds ago,
+            # wait until the 60 seconds passed
+            if diff < 60:
+                sleep_time = 60 - diff
+                time.sleep(sleep_time)
