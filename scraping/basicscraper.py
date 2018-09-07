575ced43c8d0cb75385c9e9b89562e51bdb6ba1d
Rico <d-Rickyy-b@users.noreply.github.com>
2018-09-08 01:56:41 +0200

    TASK: Improve basic scraper

--- scraping/basicscraper.py
+++ scraping/basicscraper.py
@@ -1,11 +1,28 @@
 # -*- coding: utf-8 -*-
+from threading import Event
+from time import sleep
 
 
 class BasicScraper(object):
     name = "BasicScraper"
 
-    def __init__(self):
-        pass
+    def __init__(self, exception_event=None):
+        self.running = False
+        self.__stop_event = Event()
+        self.__exception_event = exception_event or Event()
+
+    def _check_stop_event(self):
+        return self.__stop_event.is_set()
+
+    def _check_exception_event(self):
+        return self.__exception_event.is_set()
 
     def start(self, paste_queue):
         raise NotImplementedError
+
+    def stop(self):
+        if self.running:
+            self.__stop_event.set()
+            while self.running:
+                sleep(0.1)
+            self.__stop_event.clear()
