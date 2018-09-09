7039ed7752676917c66f1f6c26166acdf9fe7c9b
Mxrk <Mxrk@users.noreply.github.com>
2018-09-09 15:44:10 +0200

    logging

--- db/db.py
+++ db/db.py
@@ -1,14 +1,14 @@
 import pymongo
+import logging
 
 class Database:
 
     def __init__(self, ip="127.0.0.1", port=27017, dbname="pastepwn", collectionname="pastes"):
-        print("Database init")
+        self.logger = logging.getLogger(__name__)
+        self.logger.debug("Database init")
         self.db = pymongo.MongoClient(ip, port)
         self.db = self.db[dbname]
         self.collection = self.db[collectionname]
-        self.logger = logging.getLogger(__name__)
-
 
     def insertData(self, dict):
         self.collection.insert_one(dict)
@@ -18,6 +18,8 @@ class Database:
         return self.collection.find({key: value})
 
     #ToDo def updateData(self, )
+
+    #ToDo def deleteData(self,)
 
     def count(self, key, value):
         return self.collection.find({key : value}).count()
