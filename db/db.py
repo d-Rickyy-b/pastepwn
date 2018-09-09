476197e92e696b2c532725bde53ce2b74a052d0a
Mxrk <Mxrk@users.noreply.github.com>
2018-09-09 15:05:49 +0200

    ADD basic database structure

---
+++ db/db.py
@@ -0,0 +1,28 @@
+import pymongo
+
+class db:
+
+    def __init__(self, ip="127.0.0.1", port=27017, dbname="pastepwn", collectionname="col"):
+        print("Database init")
+        self.db = pymongo.MongoClient(ip, port)
+        self.db = self.db[dbname]
+        self.collection = self.db[collectionname]
+        self.logger = logging.getLogger(__name__)
+
+
+    def insertData(self, dict):
+        self.collection.insert_one(dict)
+
+    #ToDo return key
+    def getData(self, key, value):
+        return self.collection.find({key: value})
+
+    #ToDo def updateData(self, )
+
+    def count(self, key, value):
+        return self.collection.find({key : value}).count()
+
+    def countAll(self):
+        return self.collection.count()
+
+
