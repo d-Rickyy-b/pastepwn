bc7ff1a272bbc6478fe5a2733e4024cf239f54f6
Rico <d-Rickyy-b@users.noreply.github.com>
2018-09-11 19:13:29 +0200

    FEATURE: Implement Request class for making requests easier

---
+++ util/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+
+from .request import Request
+
+__all__ = ('Request',)
---
+++ util/request.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+import certifi
+import urllib3
+
+
+class Request(object):
+
+    def __init__(self):
+        self.http = urllib3.PoolManager(cert_reqs='CERT_REQUIRED', ca_certs=certifi.where())
+
+    def _request_wrapper(self, *args, **kwargs):
+        response = self.http.request(*args, **kwargs)
+        response_data = response.data.decode("utf-8")
+
+        return response_data
+
+    def get(self, url):
+        return self._request_wrapper("GET", url)
+
+    def post(self, url):
+        return self._request_wrapper("POST", url)
+
+    def put(self, url):
+        return self._request_wrapper("PUT", url)
+
+    def delete(self, url):
+        return self._request_wrapper("DELETE", url)
