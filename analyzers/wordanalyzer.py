ce36010211e0253b1ea755df491e06d30479e589
Rico <d-Rickyy-b@users.noreply.github.com>
2018-09-11 19:22:49 +0200

    TASK: Add a case sensitivity setting for WordAnalyzer

--- analyzers/wordanalyzer.py
+++ analyzers/wordanalyzer.py
@@ -7,11 +7,16 @@ class WordAnalyzer(BasicAnalyzer):
     """Analyzer to match the content of a paste via regular expressions"""
     name = "WordAnalyzer"
 
-    def __init__(self, action, word):
+    def __init__(self, action, word, case_sensitive=False):
         super().__init__(action)
         self.word = word
+        self.case_sensitive = case_sensitive
 
     def match(self, paste):
         """Check if the specified word is part of the paste text"""
         paste_content = paste.body
-        return self.word in paste_content
+
+        if self.case_sensitive:
+            return self.word.lower() in paste_content.lower()
+        else:
+            return self.word in paste_content
