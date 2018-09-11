6352f44b4ef1d784ad55e3d60473e0cfca0e0a4f
Rico <d-Rickyy-b@users.noreply.github.com>
2018-09-11 19:11:01 +0200

    FEATURE: Implement generic action
    With the GenericAction you can pass any function pointer to the init method to run it later when the analyzer matches a paste.

--- actions/__init__.py
+++ actions/__init__.py
@@ -4,5 +4,6 @@ from .basicaction import BasicAction
 from .savefileaction import SaveFileAction
 from .telegramaction import TelegramAction
 from .logaction import LogAction
+from .genericaction import GenericAction
 
-__all__ = ('BasicAction', 'SaveFileAction', 'TelegramAction', 'LogAction')
+__all__ = ('BasicAction', 'SaveFileAction', 'TelegramAction', 'LogAction', 'GenericAction')
