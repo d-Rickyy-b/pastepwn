83a8d5807bb8c73316073e9d315cb74a0cb949be
Rico <d-Rickyy-b@users.noreply.github.com>
2018-09-16 12:36:48 +0200

    FEATURE: Implement WebhookAction
    fixes #5

--- actions/webhookaction.py
+++ actions/webhookaction.py
@@ -1,15 +1,18 @@
 # -*- coding: utf-8 -*-
 from actions import BasicAction
+from util import Request
 
 
 class WebhookAction(BasicAction):
-    """Base class for actions which can be performed on pastes"""
+    """Action to perform a Webhook on a matched paste"""
     name = "WebhookAction"
 
-    def __init__(self):
+    def __init__(self, url):
         super().__init__()
+        self.url = url
 
     def perform(self, paste, analyzer_name=None):
         """Trigger the webhook"""
-        # TODO Add webhook
-        pass
+        # TODO - More post options ([custom] body, template, choose between GET/POST etc.)
+        r = Request()
+        r.post(url=self.url)
