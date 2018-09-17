64abb035cd58daefbe900a1b01b5aadd524ef008
Rico <d-Rickyy-b@users.noreply.github.com>
2018-09-17 22:11:15 +0200

    TASK: Improve threading utils

--- util/threadingutils.py
+++ util/threadingutils.py
@@ -1,19 +1,21 @@
 # -*- coding: utf-8 -*-
 from threading import Thread, current_thread
+import logging
 
 
-def _init_thread(self, target, name, *args, **kwargs):
-    thr = Thread(target=self._thread_wrapper, name=name, args=(target,) + args, kwargs=kwargs)
-    thr.start()
+def start_thread(target, name, exception_event, *args, **kwargs):
+    thread = Thread(target=thread_wrapper, name=name, args=(target, exception_event) + args, kwargs=kwargs)
+    thread.start()
 
 
-def _thread_wrapper(self, target, *args, **kwargs):
-    thr_name = current_thread().name
-    self.logger.debug('{0} - started'.format(thr_name))
+def thread_wrapper(target, exception_event, *args, **kwargs):
+    thread_name = current_thread().name
+    logger = logging.getLogger(__name__)
+    logger.debug('{0} - thread started'.format(thread_name))
     try:
         target(*args, **kwargs)
     except Exception:
-        self.__exception_event.set()
-        self.logger.exception('unhandled exception in %s', thr_name)
+        exception_event.set()
+        logger.exception('unhandled exception in %s', thread_name)
         raise
-    self.logger.debug('{0} - ended'.format(thr_name))
+    logger.debug('{0} - thread ended'.format(thread_name))
