ed226e3de8c0ab5cca9592423b6319d6ba39428f
Rico <d-Rickyy-b@users.noreply.github.com>
2018-09-24 13:20:56 +0200

    FEATURE: Inversion of dependency
    A user needs to pass a database object to the PastePwn class in order to use it.

--- core/pastepwn.py
+++ core/pastepwn.py
@@ -12,9 +12,9 @@ from core.pastedispatcher import PasteDispatcher
 
 class PastePwn(object):
 
-    def __init__(self, store_pastes=True, db_ip="127.0.0.1", db_port=27017):
+    def __init__(self, database=None):
         self.logger = logging.getLogger(__name__)
-        self.database = None
+        self.database = database
         self.paste_queue = Queue()
         self.action_queue = Queue()
         self.__exception_event = Event()
@@ -27,24 +27,14 @@ class PastePwn(object):
         self.action_handler = ActionHandler(action_queue=self.action_queue,
                                             exception_event=self.__exception_event)
 
-        # TODO more dynamic approach to be able to add different DBMS such as Mongo, sqlite, mysql
-        if store_pastes:
-            if db_ip is None or db_port is None:
-                self.logger.warning("No DB IP/Port specified. Not storing pastes in a database!")
-            elif db_ip is not None and db_port is not None:
-                self.logger.debug("Initalizing database")
-                try:
-                    self.database = MongoDB(ip=db_ip, port=db_port)
-                except Exception as e:
-                    self.logger.error("Exception raised while connecting to the database: {0}".format(e))
-                    self.__exception_event.set()
-                    self.database = None
-
-            if self.database is not None:
-                # Save every paste to the database with the AlwaysTrueAnalyzer
-                database_action = DatabaseAction(self.database)
-                always_true = AlwaysTrueAnalyzer(database_action)
-                self.add_analyzer(always_true)
+        if self.database is not None:
+            # Save every paste to the database with the AlwaysTrueAnalyzer
+            self.logger.info("Database provided! Storing pastes in there.")
+            database_action = DatabaseAction(self.database)
+            always_true = AlwaysTrueAnalyzer(database_action)
+            self.add_analyzer(always_true)
+        else:
+            self.logger.info("No database provided!")
 
     def add_scraper(self, scraper):
         scraper.init_exception_event(self.__exception_event)
