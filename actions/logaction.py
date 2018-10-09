2baae66551c65e4fcc4036fd4366a5fe53aa5099
Leon Sandøy <leon.sandoy@conmodo.com>
2018-10-09 23:46:01 +0200

    Dot local imports for all actions

--- actions/logaction.py
+++ actions/logaction.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import logging
 
-from actions.basicaction import BasicAction
+from .basicaction import BasicAction
 
 
 class LogAction(BasicAction):
--- actions/savefileaction.py
+++ actions/savefileaction.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import os
 
-from actions import BasicAction
+from .basicaction import BasicAction
 
 
 class SaveFileAction(BasicAction):
