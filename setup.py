e39367f80149592987194387468a3f4a37486f20
Rico <d-Rickyy-b@users.noreply.github.com>
2018-10-22 02:05:45 +0200

    TASK: Markdown description for pypi module

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
 
-packages = find_packages()
+packages = find_packages(exclude=['tests*'])
 
 
 # Taken from https://github.com/python-telegram-bot/python-telegram-bot/blob/9d99660ba95b103b3e1dc80414a5ce2fd805260b/setup.py#L9
@@ -16,14 +16,20 @@ def requirements():
         return requirements_list
 
 
+with open("README.md", "r", encoding="utf-8") as file:
+    readme = file.read()
+
+
 setup(name='pastepwn',
-      version='1.0.6',
+      version='1.0.8',
       install_requires=requirements(),
       keywords='python pastebin scraping osint framework',
       description='Python framework to scrape PasteBin pastes and analyze them',
+      long_description=readme,
+      long_description_content_type='text/markdown',
       url='https://github.com/d-Rickyy-b/pastepwn',
       author='d-Rickyy-b',
       author_email='pastepwn@rickyy.de',
       license='MIT',
       packages=packages,
-      zip_safe=False)
+      include_package_data=True)
