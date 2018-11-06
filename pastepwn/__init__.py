75e19734d486a1287157cfa3e5aa1687b66d54ee
Rico <d-Rickyy-b@users.noreply.github.com>
2018-11-07 00:08:38 +0100

    TASK: Add author string to root level init

--- pastepwn/__init__.py
+++ pastepwn/__init__.py
@@ -4,4 +4,6 @@ from .core.pastedispatcher import PasteDispatcher
 from .core.scrapinghandler import ScrapingHandler
 from .core.actionhandler import ActionHandler
 
+__author__ = "d-Rickyy-b (pastepwn@rickyy.de)"
+
 __all__ = ['PastePwn', 'Paste', 'PasteDispatcher', 'ScrapingHandler', 'ActionHandler']
