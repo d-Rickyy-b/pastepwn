ae730f0cf7380c2c9d96be947d5f20ffb8e19883
Rico <d-Rickyy-b@users.noreply.github.com>
2019-01-13 01:01:19 +0100

    TASK: Add documentation to creditcard analyzer
    also add better formatting and remane action to actions

--- pastepwn/analyzers/creditcardanalyzer.py
+++ pastepwn/analyzers/creditcardanalyzer.py
@@ -2,8 +2,14 @@
 from .regexanalyzer import RegexAnalyzer
 
 
-class  CreditCardAnalyzer(RegexAnalyzer):
-    def __init__(self, actions):
+class CreditCardAnalyzer(RegexAnalyzer):
+    """Analyzer to match Credit Cards"""
+
+    def __init__(self, action):
+        """
+        Analyzer to match Credit Cards
+        :param action: Single action or list of actions to be executed when a paste matches
+        """
         # Regex taken from https://www.regular-expressions.info/creditcard.html
         regex = r"^4[0-9]{12}(?:[0-9]{3})?$|^3[47][0-9]{13}$|" \
                 r"^(?:5[1-5][0-9]{2}|222[1-9]|22[3-9][0-9]|2[3-6][0-9]{2}|27[01][0-9]|2720)[0-9]{12}$|" \
