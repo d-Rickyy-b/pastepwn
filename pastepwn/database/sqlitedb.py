cf4996366cf27d7a98bfc06282e241129f771206
Rico <d-Rickyy-b@users.noreply.github.com>
2019-09-03 22:51:14 +0200

    FIX: Broken variable substitution
    I have no idea why I never tested this before...

--- pastepwn/database/sqlitedb.py
+++ pastepwn/database/sqlitedb.py
@@ -50,7 +50,7 @@ class SQLiteDB(AbstractDB):
 
     def _insert_data(self, paste):
         self.cursor.execute("INSERT INTO pastes (key, title, user, size, date, expire, syntax, scrape_url, full_url, body) "
-                            "VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)",
+                            "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
                             (paste.key,
                              paste.title,
                              paste.user,
