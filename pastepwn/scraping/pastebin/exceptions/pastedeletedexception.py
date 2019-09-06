071ecfd577d178637c2f50c122f5bf21979c36a0
Rico <d-Rickyy-b@users.noreply.github.com>
2019-09-06 22:17:10 +0200

    TASK: Implement several exception classes

---
+++ pastepwn/scraping/pastebin/exceptions/pastedeletedexception.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""Module for the PasteDeletedException"""
+
+
+class PasteDeletedException(Exception):
+    """Exception class indicating a paste as been deleted"""
+    pass
