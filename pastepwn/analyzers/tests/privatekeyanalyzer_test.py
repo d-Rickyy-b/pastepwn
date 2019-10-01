a8746f1a5cd8939b277c8c668dc9def334e9e620
Michal Pirchala <michal.pirchala@gmail.com>
2019-10-01 12:16:34 +0200

    #69 Private key analyzer added.

---
+++ pastepwn/analyzers/tests/privatekeyanalyzer_test.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+import unittest
+from unittest import mock
+
+from pastepwn.analyzers.privatekeyanalyzer import PrivateKeyAnalyzer
+
+
+class TestMailAnalyzer(unittest.TestCase):
+    def setUp(self):
+        self.analyzer = PrivateKeyAnalyzer(None)
+        self.paste = mock.Mock()
+
+    def test_match_positive(self):
+        """Test if positives are recognized"""
+        self.paste.body = "-----BEGIN PRIVATE KEY-----"
+        self.assertTrue(self.analyzer.match(self.paste))
+        self.paste.body = "-----BEGIN PGP PRIVATE KEY BLOCK-----"
+        self.assertTrue(self.analyzer.match(self.paste))
+        self.paste.body = "-----BEGIN RSA PRIVATE KEY-----"
+        self.assertTrue(self.analyzer.match(self.paste))
+        self.paste.body = "-----BEGIN ENCRYPTED PRIVATE KEY-----"
+        self.assertTrue(self.analyzer.match(self.paste))
+        self.paste.body = "-----BEGIN OPENSSH PRIVATE KEY-----"
+        self.assertTrue(self.analyzer.match(self.paste))
+        
+
+    def test_match_negative(self):
+        """Test if negatives are not recognized"""
+        self.paste.body = ""
+        self.assertFalse(self.analyzer.match(self.paste))
+        self.paste.body = None
+        self.assertFalse(self.analyzer.match(self.paste))
+        self.paste.body = "-----BEGIN PUBLIC KEY-----"
+        self.assertFalse(self.analyzer.match(self.paste))
+
+
+if __name__ == '__main__':
+    unittest.main()
