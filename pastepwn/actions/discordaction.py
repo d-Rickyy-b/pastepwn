a9ec528fbc7d9f39d29ac3f1a0bd4553c35882fb
Zeroji <zzeroji@gmail.com>
2019-10-06 16:33:42 +0200

    Remove unused import and clean up comments

--- pastepwn/actions/discordaction.py
+++ pastepwn/actions/discordaction.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 import logging
-import json
 from string import Template
 
 from pastepwn.util import Request, DictWrapper
@@ -8,7 +7,7 @@ from .basicaction import BasicAction
 
 
 class DiscordAction(BasicAction):
-    """Action to send a Telegram message to a certain user or group"""
+    """Action to send a Discord message to a certain webhook or channel."""
     name = "DiscordAction"
 
     def __init__(self, webhook=None, token=None, channel_id=None, custom_payload=None, template=None):
@@ -28,7 +27,7 @@ class DiscordAction(BasicAction):
             self.template = None
 
     def perform(self, paste, analyzer_name=None):
-        """Send a message via a Telegram bot to a specified user, without checking for errors"""
+        """Send a message via Discord to a specified channel, without checking for errors"""
         r = Request()
         if self.template is None:
             text = "New paste matched by analyzer '{0}' - Link: {1}".format(analyzer_name, paste.full_url)
