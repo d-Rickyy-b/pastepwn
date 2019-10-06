8481036cc664011bfa5e778a845051426426f134
Rico <d-Rickyy-b@users.noreply.github.com>
2019-10-06 12:33:11 +0200

    TASK: Implement TemplatingEngine as central component
    That way actions can easily implement templating

--- pastepwn/util/__init__.py
+++ pastepwn/util/__init__.py
@@ -3,5 +3,6 @@
 from .request import Request
 from .dictwrapper import DictWrapper
 from .threadingutils import start_thread, join_threads
+from .templatingengine import TemplatingEngine
 
-__all__ = ('Request', 'DictWrapper', 'start_thread', 'join_threads')
+__all__ = ('Request', 'DictWrapper', 'start_thread', 'join_threads', 'TemplatingEngine')
