6238d9329166f50d1467ac2a2225a05f07ac49db
Nishanth D Aluhonnu <nishanthdilip@yahoo.co.in>
2019-10-06 22:13:48 +0530

    Update sendemailaction.py

--- pastepwn/actions/sendemailaction.py
+++ pastepwn/actions/sendemailaction.py
@@ -14,20 +14,19 @@ class SendEmailAction(BasicAction):
         if not re.match(r"^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$", reciever_email) or reciever_email is None:
             raise ValueError("reciever_email not correct or None!")
         self.username = username
-        self.password=password
-        self.reciever_email=reciever_email
-        self.hostname=hostname
-        self.port=port
+        self.password =password
+        self.reciever_email = reciever_email
+        self.hostname = hostname
+        self.port = port
 
 
     def perform(self, paste, analyzer_name=None):
         address = self.username
-        password =self.password
+        password = self.password
         mailto = self.reciever_email
         msg = 'Subject: {}\n\n{}'.format("From Pastepwn", str(paste))
         mailServer = smtplib.SMTP(self.hostname , self.port)
         mailServer.starttls()
         mailServer.login(address ,password)
         mailServer.sendmail(address, mailto , msg)
-        print(" \n Sent!")
         mailServer.quit()
