3fa9236d281c444e37882710e21dafb852f87b57
Adam Jimenez <jimenez.adam0@gmail.com>
2019-10-07 14:58:36 -0400

    facebook access token analyzer

---
+++ pastepwn/analyzers/tests/facebookaccesstokenanalyzer_test.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+import unittest
+from unittest import mock
+
+from pastepwn.analyzers.facebookaccesstokenanalyzer import FacebookAccessTokenAnalyzer
+
+
+class TestFacebookAccessTokenAnalyzer(unittest.TestCase):
+    def setUp(self):
+        self.analyzer = FacebookAccessTokenAnalyzer(None)
+        self.obj = mock.Mock()
+
+    def test_match_negative(self):
+        # random string
+        self.obj.body = "Test"
+        self.assertFalse(self.analyzer.match(self.obj))
+
+        # empty
+        self.obj.body = ""
+        self.assertFalse(self.analyzer.match(self.obj))
+
+        # non-alphanumeric
+        self.obj.body = "EAACEdEose0cBA!abcdef"
+        self.assertFalse(self.analyzer.match(self.obj))
+
+        # invalid prefix
+        self.obj.body = "EAAACEdEose0cBAabcdef"
+        self.assertFalse(self.analyzer.match(self.obj))
+
+        # invalid params
+        self.obj.body = None
+        self.assertFalse(self.analyzer.match(self.obj))
+
+        self.obj = None
+        self.assertFalse(self.analyzer.match(self.obj))
+    def test_match_negative(self):
+        # random string
+        self.obj.body = "Test"
+        self.assertFalse(self.analyzer.match(self.obj))
+
+        # empty
+        self.obj.body = ""
+        self.assertFalse(self.analyzer.match(self.obj))
+
+        # non-alphanumeric
+        self.obj.body = "EAACEdEose0cBA!abcdef"
+        self.assertFalse(self.analyzer.match(self.obj))
+
+        # invalid prefix
+        self.obj.body = "EAAACEdEose0cBAabcdef"
+        self.assertFalse(self.analyzer.match(self.obj))
+
+        # invalid params
+        self.obj.body = None
+        self.assertFalse(self.analyzer.match(self.obj))
+
+        self.obj = None
+        self.assertFalse(self.analyzer.match(self.obj))
+
+    def test_match_positive(self):
+        # random valid string
+        self.obj.body = "EAACEdEose0cBA0123456789ABCDEFabcdef"
+        self.assertTrue(self.analyzer.match(self.obj))
+
+
+if __name__ == '__main__':
+    unittest.main()
