bb9bf4b09759846c25dbc3f0c9f748d166b34dff
drewgilliesbc <drew.gilliesbc@gmail.com>
2019-10-08 06:42:37 -0600

    cleaning up a little

--- pastepwn/analyzers/slacktokenanalyzer.py
+++ pastepwn/analyzers/slacktokenanalyzer.py
@@ -1,16 +1,16 @@
 # -*- coding: utf-8 -*-
-import re
-
 from .regexanalyzer import RegexAnalyzer
 
 
 class SlackTokenAnalyzer(RegexAnalyzer):
-    """Analyzer to match the content of a paste via regular expressions that look like slack tokens"""
+    """
+    Analyzer to match the content of a paste via regular expressions that look like slack tokens
+    """
     name = "SlackTokenAnalyzer"
 
     def __init__(self, actions):
         """
-        Analyzer which always matches a paste to perform actions on every paste
+        Analyzer which matches a slack token
         :param actions: A single action or a list of actions to be executed on every paste
         """
         regex = r'(xox[pboa]-[0-9]{12}-[0-9]{12}-[0-9]{12}-[a-z0-9]{32})'
