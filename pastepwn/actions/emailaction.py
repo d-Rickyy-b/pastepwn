640c92135783c1512b95daa204e455c65bd8b7b3
Rico <d-Rickyy-b@users.noreply.github.com>
2019-10-16 00:39:10 +0200

    TASK: Improve email action
    I did some improvements to the code, for example: Use context manager, extract the same regex to a variable, fixed a little bug because of the missing analyzer_name variable and a lot more

--- pastepwn/actions/emailaction.py
+++ pastepwn/actions/emailaction.py
@@ -1,41 +1,55 @@
 # -*- coding: utf-8 -*-
-from .basicaction import BasicAction
-import smtplib
 import re
+import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
+from .basicaction import BasicAction
+
+
 class EmailAction(BasicAction):
     """This action takes a username, password, receiver mail address,
-    hostname, port and when executed sends out a an
-     e-mail to the receiver containing the paste."""
-
+    hostname, port and when executed sends out an e-mail to the receiver
+    containing the paste"""
     name = "EmailAction"
 
-    def __init__(self, username, password, receiver, hostname, port):
+    def __init__(self, username, password, receiver, hostname, port=465, template=None):
         super().__init__()
-        if not re.match(r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)", username) or username is None:
+        mail_regex = r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)"
+        if username is None or not re.match(mail_regex, username):
             raise ValueError('Invalid username !')
         else:
             self.username = username
-        if not re.match(r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)", receiver) or receiver is None:
+        if receiver is None or not re.match(mail_regex, receiver):
             raise ValueError('Invalid reciever address !')
         else:
             self.receiver = receiver
         self.password = password
         self.hostname = hostname
         self.port = port
+        self.template = template
 
-    def perform(self, paste):
+    def perform(self, paste, analyzer_name=None):
+        """
+        Sends an email to the specified receiver with the paste's content
+        :param paste: The paste passed by the ActionHandler
+        :param analyzer_name: The name of the analyzer which matched the paste
+        :return: None
+        """
+        if self.template is None:
+            text = "New paste matched by analyzer '{0}' - Link: {1}".format(analyzer_name, paste.full_url)
+        else:
+            text = paste.body
+            # text = TemplatingEngine.fill_template(paste, analyzer_name, template_string=self.template)
+
         email = MIMEMultipart()
         email['From'] = self.username
         email['To'] = self.receiver
-        email['Subject'] = 'Paste sent from pastepwn'
-        email.attach(MIMEText(paste, 'plain'))
-        session = smtplib.SMTP(self.hostname, self.port)
-        session.starttls()
-        session.login(self.username, self.password)
-        text = email.as_string()
-        session.sendmail(self.username, self.receiver, text)
-        session.quit()
-        print('Email sent to {0}'.format(self.receiver))
+        email['Subject'] = 'Paste matched by pastepwn via analyzer "{}"'.format(analyzer_name)
+        email.attach(MIMEText(text, 'plain'))
+
+        # TODO there should be a way to use starttls - check https://realpython.com/python-send-email/
+        with smtplib.SMTP_SSL(self.hostname, self.port) as smtp:
+            smtp.login(self.username, self.password)
+            text = email.as_string()
+            smtp.sendmail(self.username, self.receiver, text)
