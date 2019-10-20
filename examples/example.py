96a52f08f25d5eb58bb9e47dd206149038a5bf89
Simon <14183909+psidex@users.noreply.github.com>
2019-10-20 18:13:36 +0100

    Update example to use pathlib, + more comments
    Hopefully this should be a bit more readable for beginners and should also make things a little clearer

--- examples/example.py
+++ examples/example.py
@@ -1,37 +1,40 @@
 # -*- coding: utf-8 -*-
 
 import logging.handlers
-import os
+import pathlib
 
 from pastepwn import PastePwn
 from pastepwn.actions import TelegramAction
 from pastepwn.analyzers import MailAnalyzer, WordAnalyzer
 from pastepwn.database import MongoDB
 
-# Setting up the logging
-logdir_path = os.path.dirname(os.path.abspath(__file__))
-logfile_path = os.path.join(logdir_path, "logs", "pastepwn.log")
+# Setup logging to a file in ./logs/
+logdir_path = pathlib.Path(__file__).parent.joinpath("logs").absolute()
+logfile_path = logdir_path.joinpath("pastepwn.log")
 
-if not os.path.exists(os.path.join(logdir_path, "logs")):
-    os.makedirs(os.path.join(logdir_path, "logs"))
+# Creates ./logs/ if it doesen't exist
+if not logdir_path.exists():
+    logdir_path.mkdir()
 
 logfile_handler = logging.handlers.WatchedFileHandler(logfile_path, "a", "utf-8")
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO, handlers=[logfile_handler, logging.StreamHandler()])
 
-# Framework code
+# Create a database connection and setup pastepwn
 database = MongoDB(ip="192.168.240.128")
-
 pastepwn = PastePwn(database)
 
 # Generic action to send Telegram messages on new matched pastes
 telegram_action = TelegramAction(token="token", receiver="-1001348376474")
 
+# Associate mail and word analyzers with the telegram action and add then to pastpwn
 mail_analyzer = MailAnalyzer(telegram_action)
 premium_analyzer = WordAnalyzer(telegram_action, "premium")
 
 pastepwn.add_analyzer(mail_analyzer)
 pastepwn.add_analyzer(premium_analyzer)
 
+# Start pastepwn and log that we are doing that
+logger.debug("Starting pastepwn")
 pastepwn.start()
