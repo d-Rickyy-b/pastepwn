88abf882aecb1e5c1a92ea4d9b868d02d37de687
Sree Siva Sandeep Palaparthi <palaparthi.s@husky.neu.edu>
2019-11-11 17:44:09 -0500

    feat: add tests for pastebin url analyzer (#109)
    * feat: add tests for pastebin url analyzer
    
    * fix: remove www from valid url
    
    * feat(url-analyzer): make pastbin www tests to positive
    
    * FIX: Analyzer should only catch certain links
    
    The pastebinurlanalyzer should only match on certain strings in the URL. I specified it a bit more precisely

---
+++ pastepwn/analyzers/tests/pastebinurlanalyzer_test.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+import unittest
+from unittest import mock
+
+from pastepwn.analyzers.pastebinurlanalyzer import PastebinURLAnalyzer
+
+
+class TestPastebinURLAnalyzer(unittest.TestCase):
+    def setUp(self):
+        self.analyzer = PastebinURLAnalyzer(None)
+        self.paste = mock.Mock()
+
+    def test_match_positive(self):
+        self.paste.body = "https://pastebin.com/xyz"
+        self.assertTrue(self.analyzer.match(self.paste))
+        self.paste.body = "https://pastebin.com/xyz/"
+        self.assertTrue(self.analyzer.match(self.paste))
+        self.paste.body = "http://pastebin.com/xyz"
+        self.assertTrue(self.analyzer.match(self.paste))
+        self.paste.body = "http://pastebin.com/xyz/"
+        self.assertTrue(self.analyzer.match(self.paste))
+        self.paste.body = "https://pastebin.com/xyz  "
+        self.assertTrue(self.analyzer.match(self.paste))
+        self.paste.body = "www.pastebin.com/xyz"
+        self.assertTrue(self.analyzer.match(self.paste))
+        self.paste.body = "pastebin.com/xyx"
+        self.assertTrue(self.analyzer.match(self.paste))
+        self.paste.body = "This is a pastebin URL: pastebin.com/ya249asd - and this is a test"
+
+    def test_match_negative(self):
+        self.paste.body = ""
+        self.assertFalse(self.analyzer.match(self.paste))
+        self.paste.body = None
+        self.assertFalse(self.analyzer.match(self.paste))
+        self.paste.body = "https://google.com/xyz"
+        self.assertFalse(self.analyzer.match(self.paste))
+        self.paste.body = "xyzpastebin.com/k"
+        self.assertFalse(self.analyzer.match(self.paste))
+        self.paste.body = "https://pastebin.com/"
+        self.assertFalse(self.analyzer.match(self.paste))
+
+    if __name__ == '__main__':
+        unittest.main()
