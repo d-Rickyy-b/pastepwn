2bacf25ff976b7bf73618be672acb1a74559a9db
Rico <d-Rickyy-b@users.noreply.github.com>
2020-01-26 14:26:20 +0100

    TASK: Implement regex analyzer test

---
+++ pastepwn/analyzers/tests/regexanalyzer_test.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+import unittest
+from unittest import mock
+import re
+
+from pastepwn.analyzers.regexanalyzer import RegexAnalyzer
+
+
+class TestRegexAnalyzer(unittest.TestCase):
+
+    def setUp(self):
+        """Set's up a paste mock object"""
+        self.paste = mock.Mock()
+
+    def test_matchWord(self):
+        analyzer = RegexAnalyzer(None, regex="word")
+        self.paste.body = "This text contains the word 'word'!"
+        self.assertTrue(analyzer.match(self.paste), "The regex does not match, although it should!")
+
+    def test_matchPattern(self):
+        analyzer = RegexAnalyzer(None, regex=r"\d{4}-123-\d{2}")
+        self.paste.body = "This text contains 4444-123-55 a nice pattern!"
+        self.assertTrue(analyzer.match(self.paste), "The regex does not match, although it should!")
+
+        self.paste.body = "This text contains 4657-123-57 a nice pattern!"
+        self.assertTrue(analyzer.match(self.paste), "The regex does not match, although it should!")
+
+        self.paste.body = "This text contains 465-123-57 a bad pattern!"
+        self.assertFalse(analyzer.match(self.paste), "The regex does match, although it shouldn't!")
+
+    def test_flags(self):
+        """We only test a few flags, because as long as we use regex this should work fine"""
+        analyzer = RegexAnalyzer(None, regex=r"ThIs iS mIxEd")
+        self.paste.body = "this is mixed case (not)"
+        self.assertFalse(analyzer.match(self.paste), "The regex does match, although it shouldn't!")
+
+        # Test case independend flag / ignorecase
+        analyzer = RegexAnalyzer(None, regex=r"ThIs iS mIxEd", flags=re.IGNORECASE)
+        self.paste.body = "this is mixed case (not)"
+        self.assertTrue(analyzer.match(self.paste), "The regex does not match, although it should!")
+
+        # Test multiline
+        analyzer = RegexAnalyzer(None, regex=r"^regexiscool", flags=re.MULTILINE)
+        self.paste.body = "this is a multiline string\nregexiscool is right at the start of the line\nand another line!"
+        self.assertTrue(analyzer.match(self.paste), "The regex does not match in multiline strings, although it should!")
+
+    def test_negativeMatch(self):
+        """Test a case where the regex should not match"""
+        analyzer = RegexAnalyzer(None, regex=r"[a-zA-Z]{15}")
+        self.paste.body = "abcdef1ghijklmn and a lot more characters"
+        self.assertFalse(analyzer.match(self.paste), "The regex does match, although it shouldn't!")
+
+    def test_emptyBody(self):
+        """Empty bodies should not match"""
+        analyzer = RegexAnalyzer(None, regex=r"[0-9]", flags=re.MULTILINE)
+        self.paste.body = ""
+        self.assertFalse(analyzer.match(self.paste), "The regex does match, although it shouldn't!")
+
+
+if __name__ == '__main__':
+    unittest.main()
