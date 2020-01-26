3f98c5f467365a247a95400dae4764903470c786
Rico <d-Rickyy-b@users.noreply.github.com>
2020-01-26 14:26:49 +0100

    FEATURE: Implement pastetitleanalyzer

---
+++ pastepwn/analyzers/pastetitleanalyzer.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+import re
+
+from .basicanalyzer import BasicAnalyzer
+
+
+class PasteTitleAnalyzer(BasicAnalyzer):
+    """Analyzer to match Paste titles via regex """
+    name = "PasteTitleAnalyzer"
+
+    def __init__(self, actions, regex, flags=0, blacklist=None):
+        super().__init__(actions, self.name)
+        self.regex = re.compile(regex, flags)
+        self.blacklist = blacklist or []
+
+    def match(self, paste):
+        """Match the title of a paste via regex. Return true if regex matches"""
+        paste_title = paste.title or ""
+        return self.regex.search(paste_title) is not None
---
+++ pastepwn/analyzers/tests/pastetitleanalyzer_test.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+import unittest
+from unittest import mock
+import re
+
+from pastepwn.analyzers.pastetitleanalyzer import PasteTitleAnalyzer
+
+
+class TestPasteTitleAnalyzer(unittest.TestCase):
+    def setUp(self):
+        """Set's up a paste mock object"""
+        self.paste = mock.Mock()
+
+    def test_matchWord(self):
+        analyzer = PasteTitleAnalyzer(None, regex=r"word")
+        self.paste.title = "This text contains the word 'word'!"
+        self.assertTrue(analyzer.match(self.paste), "Title does not match, although it should!")
+
+    def test_matchPattern(self):
+        analyzer = PasteTitleAnalyzer(None, regex=r"\d{2}-\d{2}-\d{4}")
+        self.paste.title = "This is a date 24-01-2020 inside the title"
+        self.assertTrue(analyzer.match(self.paste), "Title does not match pattern!")
+
+    def test_matchPattern2(self):
+        analyzer = PasteTitleAnalyzer(None, regex=r"^The title")
+        self.paste.title = "The title is cool!"
+        self.assertTrue(analyzer.match(self.paste), "Title does not match pattern!")
+
+    def test_flags(self):
+        """We only test a few flags, because as long as we use regex this should work fine"""
+        analyzer = PasteTitleAnalyzer(None, regex=r"ThIs iS mIxEd")
+        self.paste.title = "this is mixed case (not)"
+        self.assertFalse(analyzer.match(self.paste), "The regex does match, although it shouldn't!")
+
+        # Test case independend flag / ignorecase
+        analyzer = PasteTitleAnalyzer(None, regex=r"ThIs iS mIxEd", flags=re.IGNORECASE)
+        self.paste.title = "this is mixed case (not)"
+        self.assertTrue(analyzer.match(self.paste), "The regex does not match, although it should!")
+
+        # Test multiline
+        analyzer = PasteTitleAnalyzer(None, regex=r"^regexiscool", flags=re.MULTILINE)
+        self.paste.title = "this is a multiline string\nregexiscool is right at the start of the line\nand another line!"
+        self.assertTrue(analyzer.match(self.paste), "The regex does not match in multiline strings, although it should!")
+
+    def test_emptyTitle(self):
+        analyzer = PasteTitleAnalyzer(None, regex=r"Any \d pattern")
+        self.paste.title = ""
+        self.assertFalse(analyzer.match(self.paste), "Empty title does match pattern, although it  shouldn't!")
+
+
+if __name__ == '__main__':
+    unittest.main()
