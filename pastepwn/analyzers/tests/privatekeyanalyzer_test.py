85260606b5c5f2146e10e7577e6a01ef6499f997
Rico <d-Rickyy-b@users.noreply.github.com>
2020-01-31 00:29:16 +0100

    fix(analyzers): Improve regex of Priv Key analyzer
    Also update the test cases

--- pastepwn/analyzers/tests/privatekeyanalyzer_test.py
+++ pastepwn/analyzers/tests/privatekeyanalyzer_test.py
@@ -12,15 +12,18 @@ class TestPrivateKeyAnalyzer(unittest.TestCase):
 
     def test_match_positive(self):
         """Test if positives are recognized"""
-        self.paste.body = "-----BEGIN PRIVATE KEY-----"
+        self.paste.body = "-----BEGIN PRIVATE KEY-----asdasdasdfgsdfgdfgs-----END PRIVATE KEY-----"
         self.assertTrue(self.analyzer.match(self.paste))
-        self.paste.body = "-----BEGIN PGP PRIVATE KEY BLOCK-----"
+        self.paste.body = "-----BEGIN PGP PRIVATE KEY BLOCK-----ZzEF0RDWWYb1vPTIeNrKrMEOZ+LesRil6TXCn5/4I0VinlrnVpRbF4xtsVt4eIRkomN9eN-----END PGP PRIVATE " \
+                          "KEY BLOCK-----"
         self.assertTrue(self.analyzer.match(self.paste))
-        self.paste.body = "-----BEGIN RSA PRIVATE KEY-----"
+        self.paste.body = "-----BEGIN RSA PRIVATE KEY-----sRefW7LzPMmTgqHgbNv/gTtT4V/Tc1CiYGVtG9NPlP9tAAAIAI/WvZ3Clh+B0X9mkZ9-----END RSA PRIVATE KEY-----"
         self.assertTrue(self.analyzer.match(self.paste))
-        self.paste.body = "-----BEGIN ENCRYPTED PRIVATE KEY-----"
+        self.paste.body = "-----BEGIN ENCRYPTED PRIVATE KEY-----sRefW7LzPMmTgqHgbNv/gTtT4V/Tc1CiYGVtG9NPlP9tAAAIAI/WvZ3Clh+B0X9mkZ9-----END ENCRYPTED " \
+                          "PRIVATE KEY-----"
         self.assertTrue(self.analyzer.match(self.paste))
-        self.paste.body = "-----BEGIN OPENSSH PRIVATE KEY-----"
+        self.paste.body = "-----BEGIN OPENSSH PRIVATE KEY-----sRefW7LzPMmTgqHgbNv/gTtT4V/Tc1CiYGVtG9NPlP9tAAAIAI/WvZ3Clh+B0X9mkZ9-----END OPENSSH " \
+                          "PRIVATE KEY-----"
         self.assertTrue(self.analyzer.match(self.paste))
 
     def test_match_negative(self):
