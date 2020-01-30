464ad9f32e56c36044f076f39e526b3cd31b77fa
Rico <d-Rickyy-b@users.noreply.github.com>
2020-01-31 00:30:11 +0100

    fix(analyzers): Fix added capture group

--- pastepwn/analyzers/slacktokenanalyzer.py
+++ pastepwn/analyzers/slacktokenanalyzer.py
@@ -13,5 +13,5 @@ class SlackTokenAnalyzer(RegexAnalyzer):
         Analyzer which matches a slack token
         :param actions: A single action or a list of actions to be executed on every paste
         """
-        regex = r'(xox[pboa]-[0-9]{12}-[0-9]{12}-[0-9]{12}-[a-z0-9]{32})'
+        regex = r'xox[pboa]-[0-9]{12}-[0-9]{12}-[0-9]{12}-[a-z0-9]{32}'
         super().__init__(actions, regex)
--- pastepwn/analyzers/stripeapikeyanalyzer.py
+++ pastepwn/analyzers/stripeapikeyanalyzer.py
@@ -5,5 +5,5 @@ from .regexanalyzer import RegexAnalyzer
 class StripeApiKeyAnalyzer(RegexAnalyzer):
 
     def __init__(self, actions):
-        regex = r'(?:r|s)k_(live|test)_[0-9a-zA-Z]{24}'
+        regex = r'(?:r|s)k_(?:live|test)_[0-9a-zA-Z]{24}'
         super().__init__(actions, regex)
