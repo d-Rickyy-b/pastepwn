88b8646cb3a7bac5cf8d5efa9bf0557f179ff272
Rico <d-Rickyy-b@users.noreply.github.com>
2020-01-31 19:41:12 +0100

    fix(analyzers): Use non-capturing group for awssessiontokenanalyzer

--- pastepwn/analyzers/awssessiontokenanalyzer.py
+++ pastepwn/analyzers/awssessiontokenanalyzer.py
@@ -9,5 +9,5 @@ class AWSSessionTokenAnalyzer(RegexAnalyzer):
     name = "AWSSessionTokenAnalyzer"
 
     def __init__(self, actions):
-        regex = r"((\\\"|'|`)?((i)?aws)?_?((i)?session)?_?((i)?token)?(\\\"|'|`)?\\\\s{0,50}(:|=>|=)\\\\s{0,50}(\\\"|'|`)?[A-Za-z0-9/+=]{16,}(\\\"|'|`)?)"
+        regex = r"(?:(?:\\\"|'|`)?(?:i?aws)?_?(?:i?session)?_?(?:i?token)?(?:\\\"|'|`)?\\\\s{0,50}(?::|=>|=)\\\\s{0,50}(?:\\\"|'|`)?[A-Za-z0-9/+=]{16,}(?:\\\"|'|`)?)"
         super().__init__(actions, regex)
