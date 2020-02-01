a0bee4645d0a9d66e309969525adcea992c553f8
Rico <d-Rickyy-b@users.noreply.github.com>
2020-02-01 01:41:26 +0100

    fix(analyzers): Make sure phonenumberanalyzer actually matches
    Before the regex did only match the country code which was not sufficient

--- pastepwn/analyzers/phonenumberanalyzer.py
+++ pastepwn/analyzers/phonenumberanalyzer.py
@@ -8,11 +8,16 @@ class PhoneNumberAnalyzer(RegexAnalyzer):
     def __init__(self, action):
         """Analyzer to match international phone numbers"""
         # from stackoverflow: https://stackoverflow.com/questions/2113908/what-regular-expression-will-match-valid-international-phone-numbers
-        country_code_regex = r"\+(9[976]\d|8[987530]\d|6[987]\d|5[90]\d|42\d|3[875]\d|2[98654321]\d|9[8543210]|8[6421]|6[6543210]|5[87654321]|4[987654310]|3[9643210]|2[70]|7|1)"
-
-        # adapted from the same stackoverflow
-        national_number = r"(\W*\d){2,17}"
-
-        regex = r"(^|\s)" + country_code_regex + r"\s*" + national_number + r"($|\s)"
+        regex = r"\+(?:9[976]\d|8[987530]\d|6[987]\d|5[90]\d|42\d|3[875]\d|2[98654321]\d|9[8543210]|8[6421]|6[6543210]|5[87654321]|4[987654310]|3[" \
+                r"9643210]|2[70]|7|1)[\d \-\(\)]{2,16}(?!\d)"
 
         super().__init__(action, regex)
+
+    def verify(self, results):
+        """Verify possible phone numbers"""
+        # Currently we don't really do verifications - we only replace spaces in the end,
+        # because our regex also matches whitespace in the end
+        verified_numbers = []
+        for result in results:
+            verified_numbers.append(result.rstrip())
+        return verified_numbers
