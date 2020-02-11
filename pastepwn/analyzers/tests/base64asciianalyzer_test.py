c4cdea825ae4e6a76df3983a613acbcd3ac214a7
jonahrosenblum <jonaherosenblum@gmail.com>
2020-02-11 14:00:18 -0500

    update base64asciianalyzer tests for decode flag functionality

--- pastepwn/analyzers/tests/base64asciianalyzer_test.py
+++ pastepwn/analyzers/tests/base64asciianalyzer_test.py
@@ -119,6 +119,43 @@ class TestBase64AsciiAnalyzer(unittest.TestCase):
         self.paste.body = "zpfDiM67zrvOuCDPicOWzpPOu8OQ"
         self.assertFalse(self.analyzer.match(self.paste))
 
+        # base64 encoded string containing one non-ascii character: "º" (UTF-8)
+        self.paste.body = "wro="
+        self.assertFalse(self.analyzer.match(self.paste))
+
+    def test_ascii_decode(self):
+        """Test if ascii decode flag works"""
+        analyzer = Base64AsciiAnalyzer(None, decode=True)
+
+        # base64 encoded string: "Hello World" (UTF-8, LF)
+        self.paste.body = "SGVsbG8gV29ybGQ="
+        self.assertEqual(analyzer.match(self.paste)[0], "Hello World")
+
+        # base64 encoded string: "Hello\nWorld" (UTF-8, LF)
+        self.paste.body = "SGVsbG9cbldvcmxk"
+        self.assertEqual(analyzer.match(self.paste)[0], "Hello\\nWorld")
+
+        # base64 encoded string (32 chars): "2fwZ_CTjDKxu48FLCLZcGdB!sEj5XRQh" (UTF-8, LF)
+        self.paste.body = "MmZ3Wl9DVGpES3h1NDhGTENMWmNHZEIhc0VqNVhSUWg="
+        self.assertEqual(analyzer.match(self.paste)[0], "2fwZ_CTjDKxu48FLCLZcGdB!sEj5XRQh")
+
+        # base64 encoded string (64 chars): "Mv=ZH?NJrrBSdhus*KVg%4dG6*C&ub?sSeq!VrzCb_-QcY^KWfxKy8AJ3=^5?b6N"
+        # (UTF-8, LF)
+        self.paste.body = "TXY9Wkg/TkpyckJTZGh1cypLVmclNGRHNipDJnViP3NTZXEhVnJ6Q2JfLVFjWV5LV2Z4S3k4QUozPV41P2I2Tg=="
+        self.assertEqual(analyzer.match(self.paste)[0], "Mv=ZH?NJrrBSdhus*KVg%4dG6*C&ub?sSeq!VrzCb_-QcY^KWfxKy8AJ3=^5?b6N")
+
+        # base64 encoded string (256 chars): "etFk!?m@A_vvdMT39Mgcynx_AFz6HY!4R8U3n_7JA?-rF=F3ehWat%4rKfhsuCc98G
+        # =t8jMY7hgJDZ2c!y!$!XQATbk6fQD2pa+EdQ_rfP^&_DKJ34dFPcuGjDBTqdxZ&=3U%@dm&?JW#+k@mB%a3TFn%GAzukL+-%TUTq?fAbAKr
+        # @y%LPK+KEmxeh+rg7?s3aR2v5A%tbn&_7zNMckCPRd&s8$wW5Bec@aRMCs@4rn?cRx?a&y-Z%kn&h8aLu*R" (UTF-8, LF)
+        self.paste.body = "ZXRGayE/bUBBX3Z2ZE1UMzlNZ2N5bnhfQUZ6NkhZITRSOFUzbl83SkE/LXJGPUYzZWhXYXQlNHJLZmhzdUNjO" \
+                          "ThHPXQ4ak1ZN2hnSkRaMmMheSEkIVhRQVRiazZmUUQycGErRWRRX3JmUF4mX0RLSjM0ZEZQY3VHakRCVHFkeF" \
+                          "omPTNVJUBkbSY/SlcjK2tAbUIlYTNURm4lR0F6dWtMKy0lVFVUcT9mQWJBS3JAeSVMUEsrS0VteGVoK3JnNz9" \
+                          "zM2FSMnY1QSV0Ym4mXzd6Tk1ja0NQUmQmczgkd1c1QmVjQGFSTUNzQDRybj9jUng/YSZ5LVola24maDhhTHUqUg=="
+        self.assertEqual(analyzer.match(self.paste)[0], "etFk!?m@A_vvdMT39Mgcynx_AFz6HY!4R8U3n_7JA?-rF=F3ehWat%4rKfhsuCc98G" \
+                                                        "=t8jMY7hgJDZ2c!y!$!XQATbk6fQD2pa+EdQ_rfP^&_DKJ34dFPcuGjDBTqdxZ&=3U%" \
+                                                        "@dm&?JW#+k@mB%a3TFn%GAzukL+-%TUTq?fAbAKr@y%LPK+KEmxeh+rg7?s3aR2v5A%tbn&" \
+                                                        "_7zNMckCPRd&s8$wW5Bec@aRMCs@4rn?cRx?a&y-Z%kn&h8aLu*R")
+
 
 if __name__ == '__main__':
     unittest.main()
