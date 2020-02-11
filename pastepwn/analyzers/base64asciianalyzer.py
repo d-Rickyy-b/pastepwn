4a21446b324baaa07ca35deab6144b8e960fedd3
Rico <d-Rickyy-b@users.noreply.github.com>
2020-02-11 19:35:08 +0100

    fix: add check for valid ascii characters
    by using the decode method on the byte string and catching exceptions, we can figure out if the result is actual ascii or other data.

--- pastepwn/analyzers/base64asciianalyzer.py
+++ pastepwn/analyzers/base64asciianalyzer.py
@@ -21,11 +21,22 @@ class Base64AsciiAnalyzer(Base64Analyzer):
         base64_ascii_strings = []
 
         for validated_string in validated_strings:
+            # Check if the string is valid base64
             try:
                 decoded_string = b64decode(validated_string)
-                if decoded_string.isascii():
-                    base64_ascii_strings.append(validated_string)
             except binascii.Error:
+                # The string is no valid base64
                 continue
+
+            # Check if the valid base64 decodes to plain ascii
+            try:
+                b64_ascii_string = decoded_string.decode('ascii')
+            except UnicodeDecodeError:
+                continue
+
+            if self.decode:
+                base64_ascii_strings.append(b64_ascii_string)
+            else:
+                base64_ascii_strings.append(validated_string)
 
         return base64_ascii_strings
