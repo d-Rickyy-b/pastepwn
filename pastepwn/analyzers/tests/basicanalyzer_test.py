987b0838b0fedd9bcd6b264a4d8f64e26273fef8
Rico <d-Rickyy-b@users.noreply.github.com>
2020-02-28 22:30:21 +0100

    test(analyzers): implement unit test for #175
    Using MagicMock, which can easily immitate another class by using the spec attribute

--- pastepwn/analyzers/tests/basicanalyzer_test.py
+++ pastepwn/analyzers/tests/basicanalyzer_test.py
@@ -2,23 +2,40 @@
 import unittest
 from unittest import mock
 
+from pastepwn.actions.basicaction import BasicAction
 from pastepwn.analyzers.basicanalyzer import BasicAnalyzer
+from pastepwn.errors import InvalidActionError
 
 
 class TestBasicAnalyzer(unittest.TestCase):
     def setUp(self):
         self.analyzer = BasicAnalyzer(None)
-        self.mock_action = mock.Mock()
+        self.mock_action = mock.MagicMock(spec=BasicAction)
 
     def test_add_action(self):
         """Check if it's possible to add actions to an analyzer"""
-        mock_action2 = mock.Mock
+        mock_action2 = mock.MagicMock(spec=BasicAction)
         analyzer = BasicAnalyzer(self.mock_action)
         self.assertEqual([self.mock_action], analyzer.actions)
 
         analyzer.add_action(mock_action2)
         self.assertTrue(isinstance(analyzer.actions, list))
         self.assertEqual([self.mock_action, mock_action2], analyzer.actions)
+
+    def test_add_action_negative(self):
+        """Check if an exception raises when passing crap as action"""
+        analyzer = BasicAnalyzer(self.mock_action)
+        self.assertEqual([self.mock_action], analyzer.actions)
+
+        with self.assertRaises(InvalidActionError, msg="BasicAnalyzer accepts objects other than BasicAnalyzers as actions in add_action!"):
+            # The mock_action2 is not a subclass of BasicAction and hence it must throw an error
+            mock_action2 = mock.Mock()
+            analyzer.add_action(mock_action2)
+
+        with self.assertRaises(InvalidActionError, msg="BasicAnalyzer accepts classes as actions in add_action!"):
+            # The mock_action3 is not even an object (only a class reference)
+            mock_action3 = mock.Mock
+            analyzer.add_action(mock_action3)
 
     def test_match(self):
         """Check if an exception is raised when trying to call the match function"""
@@ -27,30 +44,33 @@ class TestBasicAnalyzer(unittest.TestCase):
 
     def test_initialization(self):
         """Check if the initialization of the BasicAnalyzer works as intended"""
-        actions = mock.Mock()
-        analyzer = BasicAnalyzer(actions)
-        self.assertEqual([actions], analyzer.actions)
+        analyzer = BasicAnalyzer(self.mock_action)
+        self.assertEqual([self.mock_action], analyzer.actions)
 
     def test_empty_initialization(self):
+        """Check if initializing an analyzer without actions is possible"""
         analyzer = BasicAnalyzer(None)
         self.assertEqual([], analyzer.actions)
 
     def test_single_initialization(self):
+        """Check if initializing an analyzer with a single action is possible"""
         analyzer = BasicAnalyzer(self.mock_action)
         self.assertEqual([self.mock_action], analyzer.actions)
 
     def test_multi_initialization(self):
-        mock_action2 = mock.Mock()
+        """Check if initializing an analyzer with multiple actions is possible"""
+        mock_action2 = mock.MagicMock(spec=BasicAction)
         actions = [self.mock_action, mock_action2]
         analyzer = BasicAnalyzer(actions)
         self.assertEqual([self.mock_action, mock_action2], analyzer.actions)
 
     def test_logical_operators(self):
+        """Check if using logical operators on analyzers works as intended"""
         analyzer = self.analyzer & self.analyzer
         self.assertTrue(isinstance(analyzer, BasicAnalyzer))
 
-        mock_action1 = mock.Mock()
-        mock_action2 = mock.Mock()
+        mock_action1 = mock.MagicMock(spec=BasicAction)
+        mock_action2 = mock.MagicMock(spec=BasicAction)
         analyzer1 = BasicAnalyzer(mock_action1)
         analyzer2 = BasicAnalyzer(mock_action2)
 
@@ -59,6 +79,13 @@ class TestBasicAnalyzer(unittest.TestCase):
         self.assertIn(mock_action1, and_analyzer.actions)
         self.assertIn(mock_action2, and_analyzer.actions)
         self.assertEqual("(BasicAnalyzer && BasicAnalyzer)", and_analyzer.identifier)
+
+    def test_init_action_class(self):
+        """Check if an exception is being raised when a passed action is a reference to a class"""
+        # Test created based on #175
+        # If users pass a class instead of an instance, they should receive an exception
+        with self.assertRaises(InvalidActionError, msg="BasicAnalyzer accepts classes as actions!"):
+            _ = BasicAnalyzer(mock.Mock)
 
 
 if __name__ == '__main__':
