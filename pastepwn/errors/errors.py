117301c7e037294c110a35969573a150edd999d9
Rico <d-Rickyy-b@users.noreply.github.com>
2020-02-28 22:16:01 +0100

    fix(analyzers): check type of passed actions
    prior to this fix it was possible to pass all kind of stuff to the analyzers which eventually lead to #175. This commit fixes #175 by checking the type of the passed action for both the init function as well as the add_action method

---
+++ pastepwn/errors/errors.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+
+class PastepwnError(Exception):
+    """Representation of a pastepwn error object."""
+    def __init__(self, message):
+        super().__init__(message)
+        self.message = message
+
+    def __str__(self):
+        return '%s' % self.message
+
+
+class InvalidActionError(PastepwnError):
+    """Representation of an error for invalid actions passed to analyzers"""
+    pass
