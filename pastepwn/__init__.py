27b238f21e6dfa5da26badba25842f7710e964e1
Rico <d-Rickyy-b@users.noreply.github.com>
2020-06-07 17:17:05 +0200

    docs: update mail domain

--- pastepwn/__init__.py
+++ pastepwn/__init__.py
@@ -8,6 +8,6 @@ from .core.pastedispatcher import PasteDispatcher
 from .core.scrapinghandler import ScrapingHandler
 from .core.actionhandler import ActionHandler
 
-__author__ = "d-Rickyy-b (pastepwn@rickyy.de)"
+__author__ = "d-Rickyy-b (pastepwn@rico-j.de)"
 
 __all__ = ['PastePwn', 'Paste', 'PasteDispatcher', 'ScrapingHandler', 'ActionHandler']
