1be6fd7132b6deaeb4a6dc32a209fba380f3967a
Rico <d-Rickyy-b@users.noreply.github.com>
2020-12-27 02:06:27 +0100

    refactor: change line endings to LF
    Also use double quotes instead of single ones.

--- pastepwn/analyzers/tests/creditcardanalyzer_test.py
+++ pastepwn/analyzers/tests/creditcardanalyzer_test.py
@@ -70,5 +70,5 @@ class TestCreditCardAnalyzer(unittest.TestCase):
         self.assertFalse(self.analyzer.match(self.paste))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
