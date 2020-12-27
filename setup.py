09625137f48ef3a47efc048668f879abe7e7560e
Rico <d-Rickyy-b@users.noreply.github.com>
2020-12-27 04:08:00 +0100

    refactor(setup.py): use double quoted strings

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import os
 from setuptools import setup, find_packages
 
 setup_path = os.path.dirname(os.path.abspath(__file__))
-packages = find_packages(exclude=['tests*'])
+packages = find_packages(exclude=["tests*"])
 
 
 # Taken from https://github.com/python-telegram-bot/python-telegram-bot/blob/9d99660ba95b103b3e1dc80414a5ce2fd805260b/setup.py#L9
@@ -31,38 +31,39 @@ if travis_tag is not None:
 else:
     # Taken from https://packaging.python.org/guides/single-sourcing-package-version/
     version_dict = {}
-    version_file = os.path.join(setup_path, 'pastepwn', 'version.py')
+    version_file = os.path.join(setup_path, "pastepwn", "version.py")
     with open(version_file, "r", encoding="utf-8") as file:
         exec(file.read(), version_dict)
-    version = version_dict['__version__']
+    version = version_dict["__version__"]
 
 print("Building version {} of pastepwn".format(version))
 
-setup(name='pastepwn',
+setup(name="pastepwn",
       version=version,
       install_requires=requirements(),
-      keywords='python pastebin scraping osint framework',
-      description='Python framework to scrape PasteBin pastes and analyze them',
+      keywords="python pastebin scraping osint framework",
+      description="Python framework to scrape PasteBin pastes and analyze them",
       long_description=readme,
-      long_description_content_type='text/markdown',
-      url='https://github.com/d-Rickyy-b/pastepwn',
-      author='d-Rickyy-b',
-      author_email='pastepwn@rico-j.de',
-      license='MIT',
+      long_description_content_type="text/markdown",
+      url="https://github.com/d-Rickyy-b/pastepwn",
+      author="d-Rickyy-b",
+      author_email="pastepwn@rico-j.de",
+      license="MIT",
       packages=packages,
       include_package_data=True,
       classifiers=[
-          'Development Status :: 5 - Production/Stable',
-          'Environment :: Console',
-          'Intended Audience :: Developers',
-          'Intended Audience :: Science/Research',
-          'License :: OSI Approved :: MIT License',
-          'Operating System :: OS Independent',
-          'Topic :: Software Development :: Libraries :: Python Modules',
-          'Topic :: Security',
-          'Topic :: Internet',
-          'Programming Language :: Python',
-          'Programming Language :: Python :: 3.5',
-          'Programming Language :: Python :: 3.6',
-          'Programming Language :: Python :: 3.7'
-      ], )
+          "Development Status :: 5 - Production/Stable",
+          "Environment :: Console",
+          "Intended Audience :: Developers",
+          "Intended Audience :: Science/Research",
+          "License :: OSI Approved :: MIT License",
+          "Operating System :: OS Independent",
+          "Topic :: Software Development :: Libraries :: Python Modules",
+          "Topic :: Security",
+          "Topic :: Internet",
+          "Programming Language :: Python",
+          "Programming Language :: Python :: 3.5",
+          "Programming Language :: Python :: 3.6",
+          "Programming Language :: Python :: 3.7"
+          ],
+      )
