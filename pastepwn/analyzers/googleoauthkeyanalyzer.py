be9d71b047e73e887a7a2c6ac7a6841af2a82ad0
justinkiernan <72414723+justinkiernan@users.noreply.github.com>
2023-05-30 19:06:21 -0700

    Update googleoauthkeyanalyzer.py

--- pastepwn/analyzers/googleoauthkeyanalyzer.py
+++ pastepwn/analyzers/googleoauthkeyanalyzer.py
@@ -7,3 +7,11 @@ class GoogleOAuthKeyAnalyzer(RegexAnalyzer):
     def __init__(self, actions):
         regex = r"[0-9]+-[0-9A-Za-z_]{32}\.apps\.googleusercontent\.com"
         super().__init__(actions, regex)
+        
+    def __init__(self, actions):
+        regex=r"\d{10,0}-[A-Za-z]{3,}\d{3,}[A-Za-z]{3,}\d{3,}\.apps\.googleusercontent\.com"
+        super().__init__(actions, regex)
+        
+    def __init__(self, actions):
+        regex=r"\d{12,}+-[0-9A-Za-z_]{32}\.apps\.googleusercontent\.com"
+        super().__init__(actions, regex)
