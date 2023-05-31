ca593beea453c1b5bd763157e377733d63178bf9
justinkiernan <72414723+justinkiernan@users.noreply.github.com>
2023-05-30 19:12:40 -0700

    Update googleoauthkeyanalyzer.py

--- pastepwn/analyzers/googleoauthkeyanalyzer.py
+++ pastepwn/analyzers/googleoauthkeyanalyzer.py
@@ -8,10 +8,10 @@ class GoogleOAuthKeyAnalyzer(RegexAnalyzer):
         regex = r"[0-9]+-[0-9A-Za-z_]{32}\.apps\.googleusercontent\.com"
         super().__init__(actions, regex)
         
-    def __init__(self, actions):
+        # prefix string of numbers with length 10
         regex=r"\d{10,0}-[A-Za-z]{3,}\d{3,}[A-Za-z]{3,}\d{3,}\.apps\.googleusercontent\.com"
         super().__init__(actions, regex)
         
-    def __init__(self, actions):
+        # prefix string of numbers with length 12
         regex=r"\d{12,}+-[0-9A-Za-z_]{32}\.apps\.googleusercontent\.com"
         super().__init__(actions, regex)
